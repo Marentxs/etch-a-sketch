e4ed2ee47817a6b62846022bf69087012515cb78
Julian <111262742+Marentxs@users.noreply.github.com>
2025-09-22 19:27:13 -0500

    added color picker live feature

--- javascript.js
+++ javascript.js
@@ -3,12 +3,19 @@ const grid = document.getElementsByClassName("grid");
 
 const rows = 16;
 
+const colorInput = document.getElementById("colorPicker");
+let selectedColor = "#000000ff";
+
+colorInput.addEventListener("input", (event) => {
+  selectedColor = event.target.value;
+});
+
 for (let i = 0; i < rows * rows; i++) {
   const grid = document.createElement("div");
   grid.className = "grid";
 
   grid.addEventListener("mouseover", () => {
-    grid.style.backgroundColor = "black";
+    grid.style.backgroundColor = selectedColor;
   });
 
   container.appendChild(grid);
