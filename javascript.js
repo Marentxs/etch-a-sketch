06cc50b9d2b6b96d4246e6313bf125cc98981b47
Julian <111262742+Marentxs@users.noreply.github.com>
2025-09-22 19:41:37 -0500

    added click to start/stop drawing

--- javascript.js
+++ javascript.js
@@ -10,12 +10,20 @@ colorInput.addEventListener("input", (event) => {
   selectedColor = event.target.value;
 });
 
+let painting = true;
+
 for (let i = 0; i < rows * rows; i++) {
   const grid = document.createElement("div");
   grid.className = "grid";
 
+  grid.addEventListener("click", () => {
+    painting = !painting;
+  });
+
   grid.addEventListener("mouseover", () => {
-    grid.style.backgroundColor = selectedColor;
+    if (painting) {
+      grid.style.backgroundColor = selectedColor;
+    }
   });
 
   container.appendChild(grid);
