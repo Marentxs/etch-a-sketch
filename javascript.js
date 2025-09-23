2d0c11c21bfd6a6f60b99cbe2bc3d8ed20a01f5f
Julian <111262742+Marentxs@users.noreply.github.com>
2025-09-23 01:00:35 -0500

    added logic for random mode and fixed issue with click to stop functionality

--- javascript.js
+++ javascript.js
@@ -1,26 +1,17 @@
 const container = document.getElementById("container");
 const grid = document.getElementsByClassName("grid");
 
-const rows = 16;
+let rows = 16;
 
 const colorInput = document.getElementById("colorPicker");
 let selectedColor = "#000000ff";
 
 colorInput.addEventListener("input", (event) => {
   selectedColor = event.target.value;
+  randomizer = false;
 });
 
-let painting = true;
-
-function getRandomColor() {
-  const maxVal = 0xffffff;
-  let randomNumber = Math.floor(Math.random() * maxVal);
-  let hexColor = randomNumber.toString(16);
-  hexColor = hexColor.padStart(6, "0");
-  return `#${hexColor.toUpperCase()}`;
-}
-
-console.log(getRandomColor());
+let painting = false;
 
 for (let i = 0; i < rows * rows; i++) {
   const grid = document.createElement("div");
@@ -32,9 +23,27 @@ for (let i = 0; i < rows * rows; i++) {
 
   grid.addEventListener("mouseover", () => {
     if (painting) {
-      grid.style.backgroundColor = selectedColor;
+      if (randomizer) {
+        grid.style.backgroundColor = getRandomColor();
+      } else {
+        grid.style.backgroundColor = selectedColor;
+      }
     }
   });
-
   container.appendChild(grid);
 }
+
+const random = document.getElementById("random");
+
+function getRandomColor() {
+  let r = Math.floor(Math.random() * 256); // Red component (0-255)
+  let g = Math.floor(Math.random() * 256); // Green component (0-255)
+  let b = Math.floor(Math.random() * 256); // Blue component (0-255)
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+let randomizer = false;
+
+random.addEventListener("click", () => {
+  randomizer = !randomizer;
+});
